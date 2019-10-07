80b2f13023292f53bc74acb12490256781785d9f
Popesku Maxim <Maxim.Popesku@infowatch.com>
2019-10-07 13:05:07 +0300

    app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharsComponent } from './chars/chars.component';
+import { CharComponent } from './char/char.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'chars',
+    component: CharsComponent
+  },
+  {
+    path: 'char',
+    component: CharComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharsComponent } from './chars/chars.component';
+import { CharComponent } from './char/char.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharsComponent,
+    CharComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
